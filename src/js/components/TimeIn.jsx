b30f6045b70a431b2289b4a1083d7bf46015fcd8
holyfs <egcg1986@gmail.com>
2022-12-21 17:03:48 +0100

    create TimeIn.jsx component, add TimeIn.jsx inside div inside Home.js

---
+++ src/js/components/TimeIn.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+
+const TimeIn=()=>{
+    const [value, setValue] = useState(null);
+    return(<>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+        <TimePicker
+        label="Departing time"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+    </>);
+};
+export default TimeIn;
--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import DateIn from "../components/DateIn.jsx";
 import DateOut from "../components/DateOut.jsx";
 import Passengers from "../components/Passengers.jsx";
+import TimeIn from "../components/TimeIn.jsx";
 import "../../styles/Home.css";
 
 const Home = ()=>{
@@ -16,9 +17,11 @@ const Home = ()=>{
             <div className="container fluid">
                 <div className="position-absolute top-50 start-50 translate-middle mt-5 formulario mb-5">
                     <div className="d-flex flex-column justify-content-start align-items-center align-content-center mt-2 mb-2">
-                        <div className="mt-2"><DateIn /></div>
-                        <div className="mt-2"><DateOut /></div>
-                        <div className="mt-5"><Passengers /></div>
+                        <div><DateIn /></div>
+                        <div className="mt-1"><DateOut /></div>
+                        <div className="mt-3"><Passengers /></div>
+                        <div className="mt-3"><TimeIn /></div>
+                        <div>returning time</div>
                     </div>
                 </div>
             </div>
