70ea6b9bbac60de2d55c17d7115be427a73747bb
holyfs <egcg1986@gmail.com>
2022-12-23 12:40:04 +0100

    add props to fields, requiered and everything

--- src/js/components/DateIn.jsx
+++ src/js/components/DateIn.jsx
@@ -7,16 +7,16 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 const DateIn=({ changed })=>{
     const [value, setValue] = useState(null);
 
-
     return (<>
         <LocalizationProvider dateAdapter={AdapterMoment}>
             <DateTimePicker
                 label="Departing"
-                value={value}
+                value={value?value:"required"}
                 onChange={(newValue) => {
                     setValue(newValue);
                     changed(newValue);
                   }}
+/*                 renderInput={(params) => <TextField {...params} />} */
                 renderInput={(params) => <TextField {...params} />}
             />
         </LocalizationProvider></>
--- src/js/components/SearchForm.jsx
+++ src/js/components/SearchForm.jsx
@@ -1,13 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
-/* import { useForm } from "react-hook-form"; */
-/* import moment from 'moment'; */
 import DateIn from "../components/DateIn.jsx";
 import DateOut from "../components/DateOut.jsx";
 import Passengers from "../components/Passengers.jsx";
-/* import TimeIn from "../components/TimeIn.jsx";
-import TimeOut from "../components/TimeOut.jsx"; */
-/* import SearchButton from "../components/SearchButton.jsx"; */
 
 
 const SearchForm=()=>{
@@ -16,35 +11,13 @@ const SearchForm=()=>{
         date_time_depart:null,
         date_time_arrival:null,
         number_of_passengers:null,
-/*         time_in: " ",
-        time_out: " " */
     })
-/*     const { register, handleSubmit, watch, formState: { errors } } = useForm(); */
+
     const [date_time_depart, setDate_time_depart]=useState(null);
     const [date_time_arrival, setDate_time_arrival]=useState(null);
     const [number_of_passengers, setNumber_of_passengers]=useState(null);
-/*     const [time_in, setTime_in]=useState(null);
-    const [time_out, setTime_out]=useState(null); */
-/*     console.log("tiempo"+time_in);
-    console.log("tiempoOut"+time_out); */
 
 
-/*     const valueDateFormatDTDepart=(value)=>{
-        setDate_time_depart(value.format("DD"+"/"+"MM"+"/"+"YY"))
-    }
-    const valueDateFormatDTArrival=(value)=>{
-        console.log(value.format("DD"+"/"+"MM"+"/"+"YY"))
-    } */
-
-/*     const handleChanged = (e) => {
-        console.log(e)
-        setTravelInfo({
-            date_time_depart:depart,
-            date_time_arrival:arrival,
-            number_of_passengers:passengers
-        })
-        console.log(travelInfo);
-    } */
     useEffect(()=>{
         formJson(date_time_depart,date_time_arrival,number_of_passengers)
     },[])
@@ -53,7 +26,7 @@ const SearchForm=()=>{
         if(dTD.length > 0  && nOP.length > 0){
         formJson(dTD,dTA,nOP)
         actions.StoreTravelInfo(dTD,dTA,nOP)
-        } 
+        }
     }
     const formJson=(dTD,dTA,nOP)=>{
         setTravelInfo({
@@ -62,7 +35,6 @@ const SearchForm=()=>{
             number_of_passengers:nOP
         })
     }
-
     
     return(<>
         <div className="position-absolute top-50 start-50 translate-middle mt-5 formulario mb-5 pb-3 pt-1 mt-5">
@@ -70,8 +42,7 @@ const SearchForm=()=>{
                 <div><DateIn changed={(value) => setDate_time_depart(value?value.format("DD/MM/YYYY kk:mm"):value)}/></div>
                 <div className="mt-1"><DateOut changed={(value) => setDate_time_arrival(value?value.format("DD/MM/YYYY kk:mm"):value)} /></div>
                 <div className="mt-3"><Passengers changed={(value) => setNumber_of_passengers(value)} /></div>
-{/*                 <div className="mt-3"><TimeIn changed={(value) => setTime_in(value)} /></div>
-                <div className="mt-1"><TimeOut changed={(value) => setTime_out(value)} /></div> */}
+                <label>from:</label>
             </div>
             <div className="d-flex flex-column justify-content-end align-items-end align-content-end mt-5 mb-2 me-2">
 
